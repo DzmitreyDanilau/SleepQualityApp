816885909f8756a1f853f820eeef49892a2e934f
DmitryDanilov <d.danilov@vironit.co.uk>
2019-11-19 12:14:55 +0300

    +bind adapters to sleep list item +Bundings fun

--- app/src/main/java/com/example/android/trackmysleepquality/adapters/SleepNightRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/adapters/SleepNightRecyclerViewAdapter.kt
@@ -5,19 +5,11 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightRecyclerViewAdapter : ListAdapter<SleepNight,SleepItemViewHolder>(SleepNightDiffCallback()) {
-
-//    var sleepItemsList = listOf<SleepNight>()
-//        set(value) {
-//            field = value
-//            notifyDataSetChanged()
-//        }
+class SleepNightRecyclerViewAdapter : ListAdapter<SleepNight, SleepItemViewHolder>(SleepNightDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = SleepItemViewHolder.from(parent)
 
-
     override fun onBindViewHolder(holder: SleepItemViewHolder, position: Int) {
-        val resources = holder.itemView.resources
-        holder.bind(getItem(position), resources)
+        holder.bind(getItem(position))
     }
 }
